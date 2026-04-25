256c03d4818b1dbd39b866468abe3a4d2f364c92
Evgeniy Saschenko <evgeniy.saschenko@gmail.com>
2026-04-25 20:26:37 +0300

    fix-css-var

---
+++ .output/server/chunks/app/_nuxt/index-3f3546d0.mjs
@@ -0,0 +1,167 @@
+import { executeAsync } from 'unctx';
+import { b as _export_sfc, d as defineNuxtComponent, u as useNuxtApp, e as useRoute, h as useSectionsStore, f as useBreadcrumbsStore, a as useSettingsStore, g as useSeoMeta } from '../server.mjs';
+import { useSSRContext, resolveComponent, mergeProps } from 'vue';
+import { A as AppRatingsList } from './app-ratings-list-6348b474.mjs';
+import __nuxt_component_0 from './app-preloader-954716fa.mjs';
+import __nuxt_component_1 from './app-title-b41355d9.mjs';
+import { ssrRenderAttrs, ssrRenderComponent, ssrInterpolate } from 'vue/server-renderer';
+import 'ofetch';
+import 'hookable';
+import 'destr';
+import 'h3';
+import '@unhead/ssr';
+import 'unhead';
+import '@unhead/shared';
+import 'vue-router';
+import 'ufo';
+import 'vue3-lazyload';
+import 'cookie-es';
+import 'ohash';
+import 'defu';
+import '../../nitro/node-server.mjs';
+import 'node-fetch-native/polyfill';
+import 'node:http';
+import 'node:https';
+import 'unenv/runtime/fetch/index';
+import 'scule';
+import 'klona';
+import 'unstorage';
+import 'radix3';
+import 'node:fs';
+import 'node:url';
+import 'pathe';
+import './app-label-rating-cfbe1985.mjs';
+
+async function getRatingsList() {
+  let { $api } = useNuxtApp();
+  let { params, query } = useRoute();
+  let ratingsList = await $api.getPageSection({
+    sectionId: Number(params.sectionId),
+    page: Number(query.page) || 1
+  });
+  return ratingsList;
+}
+const _sfc_main = /* @__PURE__ */ defineNuxtComponent({
+  async asyncData() {
+    let __temp, __restore;
+    let { $t, $langDefault } = useNuxtApp();
+    let { params, query } = useRoute();
+    let ratingsList = ([__temp, __restore] = executeAsync(() => getRatingsList()), __temp = await __temp, __restore(), __temp);
+    if (ratingsList == null ? void 0 : ratingsList.isError) {
+      ratingsList.showError();
+    }
+    let sections = useSectionsStore();
+    let sectionCurrent = sections.itemsMap[+params.sectionId];
+    let sectionName = "";
+    let descr = "";
+    let pageText = "";
+    if (Number(query.page) > 1) {
+      pageText = `(${$t("Page")} ${query.page})`;
+    }
+    if (sectionCurrent) {
+      sectionName = `${sectionCurrent.name[$langDefault()]}`.trim();
+      descr = `${sectionCurrent.descr[$langDefault()]}`.trim();
+    }
+    useBreadcrumbsStore().setBreadcrumbs([
+      {
+        name: sectionName,
+        url: `/${$langDefault()}/section/${params.sectionId}`
+      }
+    ]);
+    let { pageTitlePrefix, pageTitleSufix } = useSettingsStore().items;
+    useSeoMeta({
+      title: `${pageTitlePrefix} 
+      ${$t("Section")}: 
+      ${sectionName} ${pageText} ${pageTitleSufix}`.trim(),
+      description: `${descr} ${pageText}`.trim()
+    });
+    return {
+      ratingsList,
+      sectionName,
+      isLoading: false,
+      descr,
+      pageText
+    };
+  },
+  data() {
+    return {
+      ratingsList: [],
+      sectionName: "",
+      isLoading: true,
+      descr: "",
+      pageText: ""
+    };
+  },
+  watch: {
+    $route: {
+      async handler(to, from) {
+        if (to.path !== from.path) {
+          this.isLoading = true;
+          return;
+        }
+        if (to.query.page === from.query.page)
+          return;
+        await this.setRatingsList();
+        let pageText = "";
+        if (Number(to.query.page) > 1) {
+          pageText = `(${this.$t("Page")} ${to.query.page})`;
+        }
+        this.pageText = pageText;
+      }
+    }
+  },
+  methods: {
+    async setRatingsList() {
+      this.isLoading = true;
+      try {
+        this.ratingsList = await getRatingsList();
+      } catch (error) {
+        console.error(error);
+        this.ratingsList = [];
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  },
+  components: {
+    AppRatingsList
+  }
+});
+function ssrRender(_ctx, _push, _parent, _attrs, $props, $setup, $data, $options) {
+  const _component_app_preloader = __nuxt_component_0;
+  const _component_app_title = __nuxt_component_1;
+  const _component_app_ratings_list = resolveComponent("app-ratings-list");
+  _push(`<div${ssrRenderAttrs(mergeProps({ class: "page page--section" }, _attrs))}>`);
+  _push(ssrRenderComponent(_component_app_preloader, {
+    isLoading: _ctx.isLoading,
+    position: "fixed"
+  }, null, _parent));
+  _push(ssrRenderComponent(_component_app_title, {
+    text: `${_ctx.$t("Section")}: ${_ctx.sectionName} ${_ctx.pageText}`
+  }, null, _parent));
+  _push(`<div class="page__ratings-list">`);
+  _push(ssrRenderComponent(_component_app_ratings_list, { ratingsList: _ctx.ratingsList }, null, _parent));
+  _push(`</div>`);
+  if (_ctx.descr) {
+    _push(`<div class="page__descr">`);
+    _push(ssrRenderComponent(_component_app_title, {
+      text: _ctx.$t("Description"),
+      level: 3,
+      textAlign: "left"
+    }, null, _parent));
+    _push(`<div>${ssrInterpolate(`${_ctx.descr} ${_ctx.pageText}`)}</div></div>`);
+  } else {
+    _push(`<!---->`);
+  }
+  _push(`</div>`);
+}
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("pages/section/index.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["ssrRender", ssrRender]]);
+
+export { index as default };
+//# sourceMappingURL=index-3f3546d0.mjs.map
--- .output/server/chunks/app/client.manifest.mjs
+++ .output/server/chunks/app/client.manifest.mjs
@@ -1,31 +1,31 @@
 const client_manifest = {
-  "_app-ratings-list.3f2bcc3f.js": {
+  "_app-ratings-list.f3df2106.js": {
     "resourceType": "script",
     "module": true,
     "css": [
-      "app-ratings-list.58e8a4ed.css"
+      "app-ratings-list.ece345f0.css"
     ],
-    "file": "app-ratings-list.3f2bcc3f.js",
+    "file": "app-ratings-list.f3df2106.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js",
       "components/app-label-rating/app-label-rating.vue"
     ]
   },
-  "app-ratings-list.58e8a4ed.css": {
-    "file": "app-ratings-list.58e8a4ed.css",
+  "app-ratings-list.ece345f0.css": {
+    "file": "app-ratings-list.ece345f0.css",
     "resourceType": "style"
   },
-  "_index.6916de0d.js": {
+  "_index.8cf4b856.js": {
     "resourceType": "script",
     "module": true,
-    "file": "index.6916de0d.js",
+    "file": "index.8cf4b856.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ]
   },
   "app-ratings-list.css": {
     "resourceType": "style",
-    "file": "app-ratings-list.58e8a4ed.css",
+    "file": "app-ratings-list.ece345f0.css",
     "src": "app-ratings-list.css"
   },
   "assets/fonts/montserrat/bold.woff2": {
@@ -49,7 +49,7 @@ const client_manifest = {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "app-label-rating.19a37322.js",
+    "file": "app-label-rating.9816e225.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ],
@@ -69,7 +69,7 @@ const client_manifest = {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "app-preloader.2598caed.js",
+    "file": "app-preloader.093a5401.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ],
@@ -82,27 +82,27 @@ const client_manifest = {
   },
   "components/app-title/app-title.css": {
     "resourceType": "style",
-    "file": "app-title.364b0651.css",
+    "file": "app-title.4900d62f.css",
     "src": "components/app-title/app-title.css"
   },
   "components/app-title/app-title.vue": {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "app-title.ff53d84e.js",
+    "file": "app-title.ea097bec.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ],
     "isDynamicEntry": true,
     "src": "components/app-title/app-title.vue"
   },
-  "app-title.364b0651.css": {
-    "file": "app-title.364b0651.css",
+  "app-title.4900d62f.css": {
+    "file": "app-title.4900d62f.css",
     "resourceType": "style"
   },
   "node_modules/nuxt/dist/app/entry.css": {
     "resourceType": "style",
-    "file": "entry.cdf95a4c.css",
+    "file": "entry.e75990cf.css",
     "src": "node_modules/nuxt/dist/app/entry.css"
   },
   "node_modules/nuxt/dist/app/entry.js": {
@@ -113,17 +113,17 @@ const client_manifest = {
       "bold.ae919a7c.woff2"
     ],
     "css": [
-      "entry.cdf95a4c.css"
+      "entry.e75990cf.css"
     ],
     "dynamicImports": [
       "virtual:nuxt:/app/.nuxt/error-component.mjs"
     ],
-    "file": "entry.b0782bab.js",
+    "file": "entry.161e82b0.js",
     "isEntry": true,
     "src": "node_modules/nuxt/dist/app/entry.js"
   },
-  "entry.cdf95a4c.css": {
-    "file": "entry.cdf95a4c.css",
+  "entry.e75990cf.css": {
+    "file": "entry.e75990cf.css",
     "resourceType": "style"
   },
   "regular.a8447cde.woff2": {
@@ -138,17 +138,17 @@ const client_manifest = {
   },
   "pages/rating/index.css": {
     "resourceType": "style",
-    "file": "index.5784865e.css",
+    "file": "index.1195dcc2.css",
     "src": "pages/rating/index.css"
   },
   "pages/rating/index.vue": {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "index.3d00bcbe.js",
+    "file": "index.332e10a3.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js",
-      "_index.6916de0d.js",
+      "_index.8cf4b856.js",
       "pages/rating/rating-items.vue",
       "pages/rating/labels-sections.vue",
       "pages/rating/links-sources.vue",
@@ -159,60 +159,60 @@ const client_manifest = {
     "isDynamicEntry": true,
     "src": "pages/rating/index.vue"
   },
-  "index.5784865e.css": {
-    "file": "index.5784865e.css",
+  "index.1195dcc2.css": {
+    "file": "index.1195dcc2.css",
     "resourceType": "style"
   },
   "pages/rating/labels-sections.css": {
     "resourceType": "style",
-    "file": "labels-sections.cc37fc9d.css",
+    "file": "labels-sections.258dbd7f.css",
     "src": "pages/rating/labels-sections.css"
   },
   "pages/rating/labels-sections.vue": {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "labels-sections.8d7d007c.js",
+    "file": "labels-sections.31650041.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ],
     "isDynamicEntry": true,
     "src": "pages/rating/labels-sections.vue"
   },
-  "labels-sections.cc37fc9d.css": {
-    "file": "labels-sections.cc37fc9d.css",
+  "labels-sections.258dbd7f.css": {
+    "file": "labels-sections.258dbd7f.css",
     "resourceType": "style"
   },
   "pages/rating/links-sources.css": {
     "resourceType": "style",
-    "file": "links-sources.fc3047c4.css",
+    "file": "links-sources.ae3ac3b2.css",
     "src": "pages/rating/links-sources.css"
   },
   "pages/rating/links-sources.vue": {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "links-sources.b597a312.js",
+    "file": "links-sources.5634e20a.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ],
     "isDynamicEntry": true,
     "src": "pages/rating/links-sources.vue"
   },
-  "links-sources.fc3047c4.css": {
-    "file": "links-sources.fc3047c4.css",
+  "links-sources.ae3ac3b2.css": {
+    "file": "links-sources.ae3ac3b2.css",
     "resourceType": "style"
   },
   "pages/rating/rating-items.css": {
     "resourceType": "style",
-    "file": "rating-items.94989540.css",
+    "file": "rating-items.b289330c.css",
     "src": "pages/rating/rating-items.css"
   },
   "pages/rating/rating-items.vue": {
     "resourceType": "script",
     "module": true,
     "css": [],
-    "file": "rating-items.3d295ea2.js",
+    "file": "rating-items.d0dd0dab.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js",
       "components/app-label-rating/app-label-rating.vue"
@@ -220,18 +220,18 @@ const client_manifest = {
     "isDynamicEntry": true,
     "src": "pages/rating/rating-items.vue"
   },
-  "rating-items.94989540.css": {
-    "file": "rating-items.94989540.css",
+  "rating-items.b289330c.css": {
+    "file": "rating-items.b289330c.css",
     "resourceType": "style"
   },
   "pages/ratings-all/index.vue": {
     "resourceType": "script",
     "module": true,
-    "file": "index.33b01a3f.js",
+    "file": "index.bebc5767.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js",
-      "_index.6916de0d.js",
-      "_app-ratings-list.3f2bcc3f.js",
+      "_index.8cf4b856.js",
+      "_app-ratings-list.f3df2106.js",
       "components/app-preloader/app-preloader.vue",
       "components/app-title/app-title.vue",
       "components/app-label-rating/app-label-rating.vue"
@@ -242,11 +242,11 @@ const client_manifest = {
   "pages/section/index.vue": {
     "resourceType": "script",
     "module": true,
-    "file": "index.cbf4946b.js",
+    "file": "index.98369663.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js",
-      "_index.6916de0d.js",
-      "_app-ratings-list.3f2bcc3f.js",
+      "_index.8cf4b856.js",
+      "_app-ratings-list.f3df2106.js",
       "components/app-preloader/app-preloader.vue",
       "components/app-title/app-title.vue",
       "components/app-label-rating/app-label-rating.vue"
@@ -256,7 +256,7 @@ const client_manifest = {
   },
   "virtual:nuxt:/app/.nuxt/error-component.css": {
     "resourceType": "style",
-    "file": "error-component.f6c99654.css",
+    "file": "error-component.dcfd6849.css",
     "src": "virtual:nuxt:/app/.nuxt/error-component.css"
   },
   "virtual:nuxt:/app/.nuxt/error-component.mjs": {
@@ -267,17 +267,17 @@ const client_manifest = {
       "bold.ae919a7c.woff2"
     ],
     "css": [
-      "error-component.f6c99654.css"
+      "error-component.dcfd6849.css"
     ],
-    "file": "error-component.129f4eed.js",
+    "file": "error-component.27893aec.js",
     "imports": [
       "node_modules/nuxt/dist/app/entry.js"
     ],
     "isDynamicEntry": true,
     "src": "virtual:nuxt:/app/.nuxt/error-component.mjs"
   },
-  "error-component.f6c99654.css": {
-    "file": "error-component.f6c99654.css",
+  "error-component.dcfd6849.css": {
+    "file": "error-component.dcfd6849.css",
     "resourceType": "style"
   }
 };
